1127ea79c1000b1e048d858273bb35b4dd97f1c6
Amiinoz <momagan@gmail.com>
2021-01-09 15:23:32 -0700

    Adding mouse interactions

--- script.js
+++ script.js
@@ -9,6 +9,20 @@ let score = 0;
 let gameFrame = 0;
 ctx.font = '50px Arial';
 
-// Capture mouse position
+// Mouse Interaction
+// get the correct mouse position
+let canvasPosition = canvas.getBoundingClientRect();
+const mouse = {
+  x: canvas.width/2,
+  y: canvas.height/2,
+  click: false
+}
+canvas.addEventListener('mousedown', function(event) {
+  mouse.x = event.x - canvasPosition.left;
+  mouse.y = event.y - canvasPosition.top;
+  // console.log(event);
+  // console.log(mouse.x, mouse.y);
+  // console.log(canvasPosition);
+})
 // Players
 // Animation loop
